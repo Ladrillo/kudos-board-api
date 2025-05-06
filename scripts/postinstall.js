f919f323ce6d6c68375accd2e1e8c95bce36a76b
Gabriel Cabrejas <gabriel@cabrejas.net>
2025-05-06 15:52:05 +0000

    another oops

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,10 +1,10 @@
+const { execSync } = require('child_process')
 if (process.env.NODE_ENV === 'production') {
-  const { execSync } = require('child_process')
   console.log('Running postinstall tasks for production...')
-  execSync('npm prisma generate', { stdio: 'inherit' })
-  execSync('npm prisma migrate deploy', { stdio: 'inherit' })
-  execSync('npm prisma db seed', { stdio: 'inherit' })
+  execSync('prisma generate', { stdio: 'inherit' })
+  execSync('prisma migrate deploy', { stdio: 'inherit' })
+  execSync('prisma db seed', { stdio: 'inherit' })
 } else {
-  execSync('npm prisma generate', { stdio: 'inherit' })
-  execSync('npm prisma db seed', { stdio: 'inherit' })
+  execSync('prisma generate', { stdio: 'inherit' })
+  execSync('prisma db seed', { stdio: 'inherit' })
 }
