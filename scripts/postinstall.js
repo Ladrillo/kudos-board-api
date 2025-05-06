6ace1d5e91bafa5675294601ec08da03ff3410b6
Gabriel Cabrejas <gabriel@cabrejas.net>
2025-05-06 15:56:25 +0000

    tweak

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,10 +1,11 @@
 const { execSync } = require('child_process')
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Running postinstall tasks for dev...')
+  execSync('prisma generate', { stdio: 'inherit' })
+  execSync('prisma db seed', { stdio: 'inherit' })
+} else {
   console.log('Running postinstall tasks for production...')
   execSync('prisma generate', { stdio: 'inherit' })
   execSync('prisma migrate deploy', { stdio: 'inherit' })
-  execSync('prisma db seed', { stdio: 'inherit' })
-} else {
-  execSync('prisma generate', { stdio: 'inherit' })
   execSync('prisma db seed', { stdio: 'inherit' })
 }
