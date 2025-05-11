83105f8c0bb4f7bf8b7fc7dc4cd89289f2db4171
Gabriel Cabrejas <gabriel@cabrejas.net>
2025-05-11 21:13:32 +0000

    css

--- frontend/src/components/BoardDetail.jsx
+++ frontend/src/components/BoardDetail.jsx
@@ -31,7 +31,7 @@ function BoardDetail() {
                 <div className='card-content col'>
                   <div className='card-info col'>
                     <h4>{card.title}</h4>
-                    <div>{card.description} {card.owner && <>({card.owner})</>}</div>
+                    <p className='clamp'>{card.description} {card.owner && <>({card.owner})</>}</p>
                   </div>
                   <div><img src={card.gif} /></div>
                 </div>
